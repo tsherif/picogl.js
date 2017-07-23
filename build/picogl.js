1a63a2dcaee983e3fda48001a717a5d89021333c
Tarek Sherif <tsherif@gmail.com>
2017-07-23 12:26:27 -0400

    Extra badges

--- build/picogl.js
+++ build/picogl.js
@@ -1,5 +1,5 @@
 /*
-PicoGL.js v0.5.6
+PicoGL.js v0.5.7
 
 The MIT License (MIT)
 
@@ -1670,7 +1670,7 @@ var App = require("./app");
     @namespace PicoGL
 */
 var PicoGL = global.PicoGL = require("./constants");    
-PicoGL.version = "0.5.6";
+PicoGL.version = "0.5.7";
 
 /**
     Create a PicoGL app. The app is the primary entry point to PicoGL. It stores
