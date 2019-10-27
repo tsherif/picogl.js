7d4af43a971b33a95658322b16b69965b7dc84c1
Tarek Sherif <tsherif@gmail.com>
2019-10-27 08:09:47 -0400

    Build cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,23 @@
-const fs = require('fs');
-const path = require('path');
 const webpack = require("webpack");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const fs = require('fs');
 
 const license = fs.readFileSync('LICENSE', { encoding: "utf8" });
 
 module.exports = {
-    entry: "./build/module/picogl.js",
     mode: "production",
+    entry: "./build/module/picogl.js",
+    plugins: [
+        new webpack.BannerPlugin({
+          banner: license
+        })
+    ],
+    devtool: "source-map",
     output: {
         library: "PicoGL",
         path: path.resolve(__dirname, "build"),
         filename: "picogl.min.js",
         libraryTarget: "umd",
         libraryExport: "PicoGL"
-    },
-    plugins: [
-        new webpack.BannerPlugin({
-          banner: license
-        })
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin({
-            sourceMap: true,
-            extractComments: false,
-            terserOptions: {
-              output: {
-                comments: /^\**!/,
-              },
-            }
-        })]
-    },
-    devtool: "source-map"
+    }
 };
